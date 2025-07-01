25b81947c76b6fee308cb010354e651cc8cdbccd
Your Name <you@example.com>
2025-07-01 18:50:17 +0300

    Fix API endpoint paths by adding trailing slashes for consistency

--- fundflow_crm/src/services/communicationService.js
+++ fundflow_crm/src/services/communicationService.js
@@ -4,55 +4,55 @@ import apiClient from './api.js';
 export const communicationService = {
   // Get all communications
   getAll: async (filters = {}) => {
-    const response = await apiClient.get('/communications', { params: filters });
+    const response = await apiClient.get('/communications/', { params: filters });
     return response.data;
   },
 
   // Get communication by ID
   getById: async (id) => {
-    const response = await apiClient.get(`/communications/${id}`);
+    const response = await apiClient.get(`/communications/${id}/`);
     return response.data;
   },
 
   // Create new communication
   create: async (communicationData) => {
-    const response = await apiClient.post('/communications', communicationData);
+    const response = await apiClient.post('/communications/', communicationData);
     return response.data;
   },
 
   // Update communication
   update: async (id, communicationData) => {
-    const response = await apiClient.put(`/communications/${id}`, communicationData);
+    const response = await apiClient.put(`/communications/${id}/`, communicationData);
     return response.data;
   },
 
   // Delete communication
   delete: async (id) => {
-    const response = await apiClient.delete(`/communications/${id}`);
+    const response = await apiClient.delete(`/communications/${id}/`);
     return response.data;
   },
 
   // Send email
   sendEmail: async (emailData) => {
-    const response = await apiClient.post('/communications/send-email', emailData);
+    const response = await apiClient.post('/communications/send-email/', emailData);
     return response.data;
   },
 
   // Send SMS
   sendSMS: async (smsData) => {
-    const response = await apiClient.post('/communications/send-sms', smsData);
+    const response = await apiClient.post('/communications/send-sms/', smsData);
     return response.data;
   },
 
   // Get communication templates
   getTemplates: async (type) => {
-    const response = await apiClient.get(`/communications/templates/${type}`);
+    const response = await apiClient.get(`/communications/templates/${type}/`);
     return response.data;
   },
 
   // Create communication template
   createTemplate: async (templateData) => {
-    const response = await apiClient.post('/communications/templates', templateData);
+    const response = await apiClient.post('/communications/templates/', templateData);
     return response.data;
   }
 };
